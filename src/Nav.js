c8e516b801908710144c888be132815527571697
John Melendez <john@Johns-MBP.domain>
2020-12-05 17:18:33 -0700

    added nav.js and nav.css

---
+++ src/Nav.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Nav() {
+  return (
+  <div className="nav">
+      <img
+        className="nav__logo"
+        scr="https://upload.wikimedia.org/wikipedia/commons/0/0f/"
+        alt="Netflix Logo"
+      />
+
+      <img
+        className="nav__avatar"
+        scr="https://pbs.twimg.com/profile_images/124011999041155"
+        alt="Netflix Logo"
+      />
+
+  </div>;
+  )
+}
+
+export default Nav;
