20ce65f73d9b73687a73a0edbab3702db0189832
John Melendez <john@Johns-MBP.domain>
2020-12-05 18:38:29 -0700

    added to nav.js

--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Nav.css";
 
 function Nav() {
+    const [show, handleShow] = useState{false};
+
   useEffect(() => {
     window.addEventListener("scroll", () => {
       if (window.scrollY > 100) {
-        // handleShow(true);
+        handleShow(true);
       } else handleShow(false);
     });
     return () => {
